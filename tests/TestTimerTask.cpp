2518dd0e85f4b1c42c1a299122a83d91928702cc
errcodex <31836909+errcodex@users.noreply.github.com>
2022-09-30 03:44:17 +0800

    Adjusted TimerTask test case.

--- tests/TestTimerTask.cpp
+++ tests/TestTimerTask.cpp
@@ -82,55 +82,6 @@ TEST(TestTimerTask, Interval)
 			return true;
 		},
 		interval, 0, true));
-	std::this_thread::sleep_for(std::chrono::seconds(3));
+	std::this_thread::sleep_for(std::chrono::seconds(1));
 	poolPtr.reset();
-}
-
-TEST(TestTimerTask, Timer)
-{
-	ASSERT_TRUE(true);
-	uint8_t times1 = 0;
-	uint8_t times2 = 0;
-	uint32_t interval1 = 1000;
-	uint32_t interval2 = 500;
-
-	// ASSERT_* 有返回值，在回调中使用回发生返回值类型不一致的问题，这里用lambda封装
-	auto lt = [](uint64_t a, uint64_t b) { ASSERT_LE(a, b); };
-
-	auto poolPtr = TimerTaskPool::create();
-	poolPtr->Add(TimerTask::create(
-		[&]() -> bool {
-			static auto last = Time::steady_time();
-			if (times1++ == 0) return true;
-
-			lt(abs(Time::timing_ms(last) - interval1), interval1 / 20);	 // 计算实际用时与interval的差
-			last = Time::steady_time();
-			return true;
-		},
-		interval1, 5, true));
-
-	poolPtr->Add(TimerTask::create(
-		[&]() -> bool {
-			static auto last = Time::steady_time();
-			if (times2++ == 0) return true;
-
-			lt(abs(Time::timing_ms(last) - interval2), interval2 / 20);	 // 计算实际用时与interval的差
-			last = Time::steady_time();
-			return true;
-		},
-		interval2, 0, true));
-
-	std::this_thread::sleep_for(std::chrono::seconds(10));
-
-	// 停止所有线程并计算停止所用时间
-	auto ts = Time::steady_time();
-	poolPtr.reset();
-	auto timeUse = Time::timing_ms(ts);
-
-	EXPECT_LE(timeUse, 50);
-
-	ASSERT_EQ(times1, 5);
-
-	ASSERT_GE(times2, 18);
-	ASSERT_LE(times2, 22);
 }
